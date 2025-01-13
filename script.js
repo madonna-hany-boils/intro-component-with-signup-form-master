c1744174446d2db0559400054344357abc73636e
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-01-13 16:54:08 +0200

    frist commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function Submit(e){
+    e.preventDefault();
+let fName=document.getElementById("firstName").value ;
+let fname_error=document.getElementById("fname-error") ;
+let lName=document.getElementById("lastName").value ;
+let lname_error=document.getElementById("lname-error") ;
+let password=document.getElementById("password").value ;
+let pass_error=document.getElementById("password-error") ;
+let email=document.getElementById("email").value ;
+let email_error=document.getElementById("email-error") ;
+
+
+    fname_error.textContent = "";
+    lname_error.textContent = "";
+
+    email_error.textContent = "";
+    pass_error.textContent = "";
+
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let flag = regex.test(email);
+    if(fName==""){
+        fname_error.textContent="First Name cann't be empty";
+        document.getElementById("firstName").classList.add("error");
+     document.getElementsByClassName("fname-error")[0].style.display="block"
+
+    }
+    if(lName==""){
+        lname_error.textContent="Last Name cann't be empty";
+        document.getElementById("lastName").classList.add("error");
+     document.getElementsByClassName("lname-error")[0].style.display="block"
+
+    }
+    if(password==""){
+        pass_error.textContent="Password cann't be empty";
+        document.getElementById("password").classList.add("error");
+     document.getElementsByClassName("pass-error")[0].style.display="block"
+
+    }
+    if(email==""){
+        email_error.textContent="Email cann't be empty ";
+        document.getElementById("email").classList.add("error");
+     document.getElementsByClassName("email-error")[0].style.display="block"
+
+    }else if(!flag){
+    email_error.textContent="Looks like this is not an email";
+     document.getElementsByClassName("email-error")[0].style.display="block"
+     document.getElementById("email").classList.add("error");
+     }
+
+}
